7d7ec302d8370e74fd78c144353216e0849257d1
Wellington Yago Gouvea <wellington652@hotmail.com>
2023-08-02 16:33:27 -0300

    fix controller

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 const handlebars = require('express-handlebars');
+const { getMovies, getMovieDetails } = require('./controllers/moviecontroller');
 
 app.get('/', (req, res) => {
   res.render('initial');
@@ -17,34 +18,19 @@ app.set('views', './views');
 
 app.post('/buscar-filme', async (req, res) => {
   const { pesquisa } = req.body;
+  let movieList = await getMovies(pesquisa);
+  console.log(movieList);
 
-  if (!pesquisa) {
-    return res.status(400).json({ error: 'O campo de pesquisa não pode estar vazio.' });
-  }
+  return res.render('buscar-filme', { movie: movieList });
 
-  try {
-    const apiKey = '2e9642f6';
-    const apiUrl = `http://www.omdbapi.com/?s=${encodeURIComponent(pesquisa)}&apikey=${apiKey}`;
-    const response = await axios.get(apiUrl);
-    return res.render('buscar-filme', { movie: response.data['Search'] });
-  } catch (error) {
-    console.error('Erro ao buscar filmes na API do OMDB:', error.message);
-    return res.status(500).json({ error: 'Erro ao buscar filmes na API do OMDB.' });
-  }
+
+
 });
 
 app.get('/details/:imdbID', async (req, res) => {
   const { imdbID } = req.params;
 
-  try {
-    const apiKey = '2e9642f6';
-    const apiUrl = `http://www.omdbapi.com/?i=${encodeURIComponent(imdbID)}&apikey=${apiKey}`;
-    const response = await axios.get(apiUrl);
-    return res.render('details', { movie: response.data });
-  } catch (error) {
-    console.error('Erro ao buscar detalhes do filme na API do OMDB:', error.message);
-    return res.status(500).json({ error: 'Erro ao buscar detalhes do filme na API do OMDB.' });
-  }
+
 });
 
 app.listen(port, () => {
