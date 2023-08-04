95b49807678f6c07580a07dce426cb013a878f65
vitorandrad3 <121817324+vitorandrad3@users.noreply.github.com>
2023-08-04 17:19:37 -0300

    Revert "feat: Fixing merging errors"
    This reverts commit c05037c3340e13c0b3f755bb44156c6581564823, reversing
    changes made to ccdefddd172e36fbeb1acbe3768fe60d2a31c54c.

--- controllers/moviecontroller.js
+++ controllers/moviecontroller.js
@@ -4,18 +4,20 @@ const apiKey = process.env.OMDB_API_KEY;
 const baseApiUrl = 'http://www.omdbapi.com/?';
 
 async function getMovies(movieName) {
-
-    if (movieName) {
-        let apiUrl = `${baseApiUrl}s=${encodeURIComponent(movieName)}&apikey=${apiKey}`;
-        const response = await axios.get(apiUrl);
-        const movieList = response.data['Search'].map((movie) => {
-            const { Title, Poster, imdbID, } = movie;
-            return new Movie(Title, Poster, imdbID);
-        });
-        console.log(movieList);
-        return movieList;
+    try {
+        if (movieName) {
+            let apiUrl = `${baseApiUrl}s=${encodeURIComponent(movieName)}&apikey=${apiKey}`;
+            const response = await axios.get(apiUrl);
+            const movieList = response.data['Search'].map((movie) => {
+                const { Title, Poster, imdbID, } = movie;
+                return new Movie(Title, Poster, imdbID);
+            });
+            return movieList;
+        }
+    } catch (error) {
+        console.error('Error while getting the list of movies:', error.message);
+        throw error;
     }
-
 }
 
 async function getMovieDetails(movieId) {
@@ -23,15 +25,15 @@ async function getMovieDetails(movieId) {
         let apiUrl = `${baseApiUrl}i=${encodeURIComponent(movieId)}&apikey=${apiKey}`;
         const response = await axios.get(apiUrl);
 
-    const {
-        Title, Poster, imdbID, Year, Plot, Genre, Director
-    } = response.data;
+        const {
+            Title, Poster, imdbID, Year, Plot, Genre, Director
+        } = response.data;
 
-    const movieWithDetails = new Movie(Title, Poster, imdbID, Year, Plot, Genre, Director);
-    console.log(movieWithDetails);
-    return movieWithDetails;
-} catch(error){
-    return null;
-}   
+        const movieWithDetails = new Movie(Title, Poster, imdbID, Year, Plot, Genre, Director);
+        return movieWithDetails;
+    } catch (error) {
+        console.error('Error while getting movie details:', error.message);
+        throw error;
+    }
 }
 module.exports = { getMovies, getMovieDetails };
