4e1b5b395b98e2e5518b5b95f9ab04c526f80d8d
vitorandrad3 <121817324+vitorandrad3@users.noreply.github.com>
2023-08-04 17:22:39 -0300

    fix errors

--- controllers/moviecontroller.js
+++ controllers/moviecontroller.js
@@ -3,20 +3,26 @@ const { Movie } = require('../models/movie');
 const apiKey = process.env.OMDB_API_KEY;
 const baseApiUrl = 'http://www.omdbapi.com/?';
 
+
 async function getMovies(movieName) {
     try {
         if (movieName) {
             let apiUrl = `${baseApiUrl}s=${encodeURIComponent(movieName)}&apikey=${apiKey}`;
             const response = await axios.get(apiUrl);
             const movieList = response.data['Search'].map((movie) => {
-                const { Title, Poster, imdbID, } = movie;
+
+                let { Title, Poster, imdbID, } = movie;
+                if (Poster === 'N/A') {
+                    Poster = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png';
+                }
                 return new Movie(Title, Poster, imdbID);
             });
             return movieList;
+
         }
     } catch (error) {
         console.error('Error while getting the list of movies:', error.message);
-        throw error;
+
     }
 }
 
@@ -24,16 +30,16 @@ async function getMovieDetails(movieId) {
     try {
         let apiUrl = `${baseApiUrl}i=${encodeURIComponent(movieId)}&apikey=${apiKey}`;
         const response = await axios.get(apiUrl);
-
-        const {
-            Title, Poster, imdbID, Year, Plot, Genre, Director
-        } = response.data;
-
+        let { Title, Poster, imdbID, Year, Plot, Genre, Director } = response.data;
+        if (Poster === 'N/A') {
+            Poster = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png';
+        }
         const movieWithDetails = new Movie(Title, Poster, imdbID, Year, Plot, Genre, Director);
         return movieWithDetails;
+
     } catch (error) {
         console.error('Error while getting movie details:', error.message);
-        throw error;
+
     }
 }
 module.exports = { getMovies, getMovieDetails };
