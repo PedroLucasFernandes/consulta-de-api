4b9d26dd0167d993914a92ee27ade24f71a0baf8
Robson Alves <alvesrobsondev@gmail.com>
2023-08-13 23:25:48 -0300

    feat: handle empty movie title in getMoviesListByTitle

--- src/controllers/moviecontroller.js
+++ src/controllers/moviecontroller.js
@@ -7,40 +7,57 @@ const { apiUrlFactory } = require('../utils/apiUrlFactory');
 
 const omdbApiKeyFactory = apiUrlFactory(omdbBaseApiUrl, apiKey);
 
-
 async function getMoviesListByTitle(movieName) {
     try {
-        const apiUrl = omdbApiKeyFactory({ s: movieName });
+        if (!movieName) {
+            throw new Error('Please provide a movie title.');
+        }
 
+        const apiUrl = omdbApiKeyFactory({ s: movieName });
         const response = await axios.get(apiUrl);
+
         if (!response.data['Search']) {
-            throw new Error('No movies found for the given title.');//Se não encontrar nenhum filme, retorna erro específico
+            throw new Error('No movies found for the given title.');
         }
 
         const movieList = response.data['Search'].map((movie) => {
-            let { Title, Poster, imdbID, } = movie;
+            let { Title, Poster, imdbID } = movie;
             const IsThereImage = Poster === 'N/A';
             if (IsThereImage) {
                 Poster = NoImageURL;
             }
             return new Movie(Title, Poster, imdbID);
-
         });
         return movieList;
-
-    }  catch (error) {
-        console.error('Error while getting the list of movies:', error.message);
-        throw error;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            if (error.response && error.response.status === 401) {
+                console.error('Invalid API key:', error.message);
+                throw new Error('Invalid API key. Please check your credentials.');
+            } else {
+                console.error('Error connecting to OMDB API:', error.message);
+                throw new Error('An error occurred while connecting to the OMDB API. Please try again later.');
+            }
+        } else {
+            console.error('Error while getting the list of movies:', error.message);
+            throw error;
+        }
     }
 }
 
+
+
 async function getMovieDetailsById(movieId) {
     try {
+        if (!movieId) {
+            throw new Error('Please provide a movie ID.');
+        }
+
         let apiUrl = omdbApiKeyFactory({ i: movieId });
         const response = await axios.get(apiUrl);
 
         if (!response.data.Title) {
-            throw new Error('No movie details found for the given ID.');//Se não encontrar nenhum filme, retorna erro específicando que foi por causa da ID 
+            throw new Error('No movie details found for the given ID.');
         }
 
         let { Title, Poster, imdbID, Year, Plot, Genre, Director } = response.data;
@@ -50,10 +67,20 @@ async function getMovieDetailsById(movieId) {
         }
         const movieWithDetails = new Movie(Title, Poster, imdbID, Year, Plot, Genre, Director);
         return movieWithDetails;
-
     } catch (error) {
-        console.error('Error while getting movie details:', error.message);
-
+        if (axios.isAxiosError(error)) {
+            if (error.response && error.response.status === 401) {
+                console.error('Invalid API key:', error.message);
+                throw new Error('Invalid API key. Please check your credentials.');
+            } else {
+                console.error('Error connecting to OMDB API:', error.message);
+                throw new Error('An error occurred while connecting to the OMDB API. Please try again later.');
+            }
+        } else {
+            console.error('Error while getting movie details:', error.message);
+            throw error;
+        }
     }
 }
-module.exports = { getMoviesListByTitle,  getMovieDetailsById };
+
+module.exports = { getMoviesListByTitle, getMovieDetailsById };
