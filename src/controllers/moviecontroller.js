d0ae3b638496c3f4937978a937309da634e31acd
Wellington Yago Gouvea <wellington652@hotmail.com>
2023-08-14 16:35:07 -0300

    feat: add switch case

--- src/controllers/moviecontroller.js
+++ src/controllers/moviecontroller.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const { Movie } = require('../models/movie');
-const apiKey = 'sdasd';
+const apiKey = process.env.OMDB_API_KEY;
 const omdbBaseApiUrl = 'http://www.omdbapi.com/';
 const NoImageURL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png';
 const { apiUrlFactory } = require('../utils/apiUrlFactory');
@@ -14,16 +14,24 @@ async function getMoviesListByTitle(movieName) {
         const apiUrl = omdbApiKeyFactory({ s: movieName });
 
         const response = await axios.get(apiUrl);
-        const movieList = response.data['Search'].map((movie) => {
-            let { Title, Poster, imdbID, } = movie;
-            const IsThereImage = Poster === 'N/A';
-            if (IsThereImage) {
-                Poster = NoImageURL;
-            }
-            return new Movie(Title, Poster, imdbID);
-
-        });
-        return movieList;
+        if(response.data['Response'] != 'False'){
+            console.log(response);
+            const movieList = response.data['Search'].map((movie) => {
+                let { Title, Poster, imdbID, } = movie;
+                const IsThereImage = Poster === 'N/A';
+                
+                if (IsThereImage) {
+                    Poster = NoImageURL;
+                
+                }
+                return new Movie(Title, Poster, imdbID);
+    
+            });
+            console.log('dfsdf');
+            return movieList;
+        }else{
+            throw new APIError(404, 'data not found');
+        }
 
     }  catch (error) {
         throw new APIError(error.response.status, error);
--- src/utils/errorMiddleware.js
+++ src/utils/errorMiddleware.js
@@ -2,7 +2,24 @@
 function errorMiddleware(err, req, res, next) { // eslint-disable-line
   console.error(err.status);
   
-  res.status(400).json({ error: 'Something went wrong' });
+  switch(err.status){
+    case 401 :
+      res.status(401).json({ error: 'Not authorized' });
+    break;
+
+    case 404 :
+      res.status(404).json({ error: 'Data not found'});
+    break;
+
+    case 503 :
+      res.status(503).json({ error: 'Service unavailable'});
+    break;
+
+      case 500 :
+        res.status(500).json({ error: 'Internal server error'});
+      break;
+  }
+ // res.status(400).json({ error: 'Something went wrong' });
 
 }
 module.exports = { errorMiddleware };
